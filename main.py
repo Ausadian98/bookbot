a9b6c95b2ef10c23983097b1ff03255140c6163a
ausadian98 <netsub@ausadian.net>
2025-01-29 00:20:06 +1030

    completion of bookbot

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
-import os
-
-with open("/home/ausadian98/git/workspace/github.com/bookbot/books/frankenstein.txt") as f:
-    file_contents = f.read()
-    lowered_string = file_contents.lower()
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
     
-    freq = {}
+    list_dict = []
+    for item in chars_dict:
+        list_dict.append({'character': item,'count': chars_dict[item]})
 
-    for letter in lowered_string:
-        if letter in freq:
-            freq[letter] += 1
-        else:
-            freq[letter] = 1
-    print(freq)
+    list_dict.sort(reverse=True, key=sort_on)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in document\n")
+    for item in list_dict:
+        print(f"The '{item["character"]}' character was found {item["count"]} times")
+    
+    print("--- End Report ---")
+
+def sort_on(dict):
+    return dict['count']
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_chars_dict(text):
+    chars = {}
+    for char in text:
+        if char.isalpha():
+            lowered = char.lower()
+            if lowered in chars:
+                chars[lowered] += 1
+            else:
+                chars[lowered] = 1
+    return chars
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+    
+main()
