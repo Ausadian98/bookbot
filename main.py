955fc42879ace16d2e5a53944619d1ba96075a67
ausadian98 <netsub@ausadian.net>
2025-02-28 20:28:56 +1030

    Refactor

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from stats import get_num_words
+
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
@@ -10,7 +13,7 @@ def main():
 
     list_dict.sort(reverse=True, key=sort_on)
     print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{num_words} words found in document\n")
+    print(f"{num_words} words found in the document\n")
     for item in list_dict:
         print(f"The '{item["character"]}' character was found {item["count"]} times")
     
@@ -18,10 +21,6 @@ def main():
 
 def sort_on(dict):
     return dict['count']
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
 
 def get_chars_dict(text):
     chars = {}
