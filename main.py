0c069bb494beab34b03a2563034d59204afce552
ausadian98 <netsub@ausadian.net>
2025-02-28 23:32:48 +1030

    solution

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from stats import *
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 
 
 def main():
-    book_path = "books/frankenstein.txt"
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
@@ -12,7 +18,7 @@ def main():
         list_dict.append({'character': item,'count': chars_dict[item]})
 
     list_dict.sort(reverse=True, key=sort_on)
-    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"--- Begin report of {sys.argv[1]} ---")
     # print(f"{num_words} words found in the document\n")
     print(f"Found {num_words} total words")
     for item in list_dict:
